cafbf4da40e4166bfe3d40f5cf3314094d6eaf7c
ecly <ecly@mailbox.org>
2017-12-23 13:39:36 +0100

    finished day 3

--- day03/triangles.hs
+++ day03/triangles.hs
@@ -1,12 +1,17 @@
 import Data.List
+import Data.List.Split
 
 main :: IO()
 main = do 
     input <- parse <$> getContents
     putStrLn $ show $ length $ filter isValidTriangle input
+    putStrLn $ show $ length $ filter isValidTriangle (adjustGroups input)
 
 parse :: String -> [[Integer]]
 parse x = map (\ l -> map read (words l)) (lines x)
+
+adjustGroups :: [[Integer]] -> [[Integer]]
+adjustGroups = chunksOf 3 . concat . transpose
 
 isValidTriangle :: [Integer] -> Bool
 isValidTriangle vals = let longest = maximum vals
