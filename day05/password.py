7e352144bb5d3d9ebc90eddf25790ddf90adb5c8
ecly <ecly@mailbox.org>
2017-12-29 21:46:48 +0100

    finished day 5

--- day05/password.py
+++ day05/password.py
@@ -1,5 +1,6 @@
 import hashlib
-def main(input):
+
+def first(input):
     output = ""
     index = 0
     while len(output) < 8:
@@ -12,4 +13,23 @@ def main(input):
 
     return output
 
-print(main('uqwqemis'))
+def second(input):
+    output = list('________')
+    index = 0
+    found = 0
+    while found < 8:
+        val = input + str(index)
+        hash = hashlib.md5(val.encode()).hexdigest()
+        if hash.startswith('00000'):
+            idx = int(hash[5], 16)
+            if idx < 8 and output[idx] == '_':
+                output[idx] = hash[6]
+                found += 1
+
+        index += 1
+
+    return ''.join(output)
+
+# about 30 seconds total
+print(first('uqwqemis'))
+print(second('uqwqemis'))
