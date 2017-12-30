c8eb0da3d0111649b5d1640beb2559b3caaeaf00
ecly <ecly@mailbox.org>
2017-12-30 15:44:32 +0100

    finished day 6

--- day06/repetition.py
+++ day06/repetition.py
@@ -1,11 +1,13 @@
-def main(file, charCount):
+def main(lines, charCount, fun):
     lists = [[] for _ in range(charCount)]
-    for line in file.readlines():
+    for line in lines:
         for i, c in enumerate(line.strip()):
             lists[i].append(c)
 
-    return ''.join(map(lambda l: max(set(l), key=l.count), lists))
+    return ''.join(map(lambda l: fun(set(l), key=l.count), lists))
 
 with open('input.in') as f:
-    print(main(f, 8))
+    lines = f.readlines()
 
+print(main(lines, 8, max))
+print(main(lines, 8, min))
