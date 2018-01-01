7ba4a702d4a980315aa6bafbe87d6ed543bf05af
ecly <ecly@mailbox.org>
2018-01-01 23:56:21 +0100

    finish day 7

--- day07/protocol.py
+++ day07/protocol.py
@@ -1,3 +1,17 @@
+def splitSequences(ip):
+    outer = []
+    inner = []
+    rem = ip[::]
+    while '[' in rem:
+        start = rem.index('[')
+        end = rem.index(']')
+        inner.append(rem[start+1:end])
+        outer.append(rem[:start])
+        rem = rem[end+1:]
+
+    outer.append(rem)
+    return outer, inner
+
 def isABBA(chars):
     return chars[0] == chars[3] and chars[1] == chars[2] and chars[0] != chars[1]
 
@@ -9,21 +23,29 @@ def hasABBA(section):
     return False
 
 def hasTLS(ip):
-    tls = []
-    hypernets = []
-    outer = ip[::]
-    while '[' in outer:
-        start = outer.index('[')
-        end = outer.index(']')
-        hypernets.append(outer[start+1:end])
-        tls.append(outer[:start])
-        outer = outer[end+1:]
+    outer, inner = splitSequences(ip)
+    return any(hasABBA(x) for x in outer) and not any(hasABBA(x) for x in inner)
 
-    tls.append(outer)
+def isABA(chars):
+    return chars[0] == chars[2] and chars[0] != chars[1]
 
-    return any(hasABBA(x) for x in tls) and not any(hasABBA(x) for x in hypernets)
+def findAllABA(section):
+    for i in range(len(section)-2):
+        if isABA(section[i:i+3]):
+            yield section[i:i+3]
+
+def hasSSL(ip):
+    outer, inner = splitSequences(ip)
+    for out in outer:
+        for aba in findAllABA(out):
+            bab = aba[1] + aba[0] + aba[1]
+            if any(bab in x for x in inner):
+                return True
+
+    return False
 
 with open('input.in') as f:
     lines = f.readlines()
 
 print(len(list(filter(hasTLS, lines))))
+print(len(list(filter(hasSSL, lines))))
