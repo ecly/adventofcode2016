71c0d527f933896867d34029f7cddd55db961bc4
ecly <ecly@mailbox.org>
2018-03-04 21:34:04 +0100

    more utility functions for day 11

--- day11/maze.hs
+++ day11/maze.hs
@@ -3,7 +3,8 @@ import Data.List
 data Object = Generator Int | Chip Int
     deriving (Show, Eq)
 
-type State = [[Object]]
+type Floor = [Object]
+type State = [Floor]
 
 input :: State
 input = [[Generator 1, Chip 1, Generator 2, Generator 3],
@@ -18,6 +19,39 @@ goal = [[Generator 1, Chip 1, Generator 2, Generator 3, Chip 2,
 
 main :: IO ()
 main = print $ bfs [] goal [input] 0
+
+isChip :: Object -> Bool
+isChip (Chip _) = True
+isChip _ = False
+
+isGenerator :: Object -> Bool
+isGenerator (Generator _) = True
+isGenerator _ = False
+
+isValidGenerator :: Object -> Object -> Bool
+isValidGenerator (Chip x) g =
+    case g of
+      Chip _ -> False
+      Generator y -> x==y
+isValidGenerator _ _ = False
+
+-- A floor is valid if all chips are connected to their Generator
+-- or there are no Generators on the floor
+isValidFloor :: Floor -> Bool
+isValidFloor floor =
+    let chips = filter isChip floor
+        hasValidGenerator x = any (isValidGenerator x) floor
+     in all hasValidGenerator chips || all isChip floor
+
+-- Compute the valid elevators that can leave a floor.
+-- This means the floor should be left valid
+-- and the elevator should not contain conflicting Objects
+validElevators :: Floor -> [Floor]
+validElevators fl =
+    let pairs = [[x,y] | (x:ys) <- tails fl, y <- ys]
+        singles = [[x] | x <- fl]
+        allElevators = filter isValidFloor $ pairs ++ singles
+     in filter (isValidFloor . (\\ fl)) allElevators
 
 neighbours :: State -> [State]
 neighbours x = [x]
