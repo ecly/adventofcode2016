b30e33d6a552ac2d85d269e600ac7deb6c8ddeab
ecly <ecly@mailbox.org>
2018-07-11 18:40:30 +0200

    finish day 21

--- day21/scramble.py
+++ day21/scramble.py
@@ -1,7 +1,12 @@
 import sys
 
-def solve(cmds):
-    val = list("abcdefgh")
+
+def rotate(l, amount):
+    return l[amount % len(l) :] + l[: amount % len(l)]
+
+
+def solve(letters, cmds):
+    val = list(letters)
     for l in cmds:
         cmd = l.split(" ")
         if cmd[0] == "swap" and cmd[1] == "position":
@@ -16,20 +21,61 @@ def solve(cmds):
             amount = 1
             if cmd[1] == "based":
                 idx = val.index(cmd[6])
-                amount += idx if idx < 4 else idx+1
+                amount += idx if idx < 4 else idx + 1
             else:
                 amount = int(cmd[2])
 
             amount = amount if cmd[1] == "left" else -amount
-            val = val[amount % len(val):] + val[:amount % len(val)]
+            val = rotate(val, amount)
 
         if cmd[0] == "reverse":
             x, y = int(cmd[2]), int(cmd[4])
-            val = val[:x] + val[x:y+1][::-1] + val[y+1:]
+            val = val[:x] + val[x : y + 1][::-1] + val[y + 1 :]
         if cmd[0] == "move":
             x, y = int(cmd[2]), int(cmd[5])
             x_val = val[x]
-            val = val[:x] + val[x+1:]
+            val = val[:x] + val[x + 1 :]
+            val.insert(y, x_val)
+
+    return "".join(val)
+
+
+def solve_rev(letters, cmds):
+    val = list(letters)
+    for l in cmds[::-1]:
+        cmd = l.split(" ")
+        if cmd[0] == "swap" and cmd[1] == "position":
+            x, y = int(cmd[2]), int(cmd[5])
+            val[x], val[y] = val[y], val[x]
+        if cmd[0] == "swap" and cmd[1] == "letter":
+            x, y = cmd[2], cmd[5]
+            x_pos, y_pos = val.index(x), val.index(y)
+            val[x_pos], val[y_pos] = val[y_pos], val[x_pos]
+        if cmd[0] == "rotate":
+            amount = 1
+            if cmd[1] == "based":
+                idx = val.index(cmd[6])
+                for a in range(0, len(val)):
+                    old = rotate(val, a)
+                    old_amount = 1
+                    old_idx = old.index(cmd[6])
+                    old_amount += old_idx if old_idx < 4 else old_idx + 1
+                    if rotate(old, -old_amount) == val:
+                        amount = -old_amount
+                        break
+            else:
+                amount = int(cmd[2])
+
+            amount = amount if cmd[1] == "right" else -amount
+            val = rotate(val, amount)
+
+        if cmd[0] == "reverse":
+            x, y = int(cmd[2]), int(cmd[4])
+            val = val[:x] + val[x : y + 1][::-1] + val[y + 1 :]
+        if cmd[0] == "move":
+            y, x = int(cmd[2]), int(cmd[5])
+            x_val = val[x]
+            val = val[:x] + val[x + 1 :]
             val.insert(y, x_val)
 
     return "".join(val)
@@ -37,4 +83,6 @@ def solve(cmds):
 
 with sys.stdin as f:
     input_ = [x.strip() for x in f.readlines()]
-    print(solve(input_))
+
+    print(solve("abcdefgh", input_))
+    print(solve_rev("fbgdceah", input_))
