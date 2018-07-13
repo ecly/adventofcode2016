cbe67339b949a66dd2517b13ba9ba5ab6803c83b
ecly <ecly@mailbox.org>
2018-07-13 23:10:42 +0200

    finish day 22

--- day22/nodes.hs
+++ day22/nodes.hs
@@ -1,15 +1,16 @@
-import Debug.Trace (trace)
-
 type Coord = (Int, Int)
 data Node = Node { coords :: Coord
-          , size :: Int
           , used :: Int
-          , available :: Int } deriving (Show, Eq)
+          , available :: Int } deriving (Show, Eq, Ord)
 
 main :: IO()
 main = do
     input <- parse <$> getContents
-    print (solve input)
+    print (solve1 input)
+    -- visualizing the grid,
+    -- helped a great lot for solving
+    -- putStrLn (visualize2 input)
+    print (solve2 input)
 
 
 parse :: String -> [Node]
@@ -28,10 +29,10 @@ coordFromFileSystem fs = (x, y)
           (x:y:_) = map (read . drop 1) xy
 
 lineToNode :: String -> Node
-lineToNode line = Node coords_ size_ used_ available_
+lineToNode line = Node coords_ used_ available_
     where (fs:rest) = words line
           coords_ = coordFromFileSystem fs
-          (size_:used_:available_:_) = map (read . init) (init rest)
+          (_:used_:available_:_) = map (read . init) (init rest)
 
 validPair :: Node -> Node -> Bool
 validPair n1 n2 = notEmpty && notSame && fits
@@ -39,5 +40,35 @@ validPair n1 n2 = notEmpty && notSame && fits
           notSame = coords n1 /= coords n2
           fits = used n1 < available n2
 
-solve :: [Node] -> Int
-solve input = sum $ map (\n -> length $ filter (validPair n) input) input
+solve1 :: [Node] -> Int
+solve1 input = sum $ map (\n -> length $ filter (validPair n) input) input
+
+visualize :: Node -> String
+visualize n
+  | coords n == (0, 0) = "S"
+  | coords n == (36, 0) = "\nG"
+  | snd (coords n) == 0 = "\n."
+  | snd (coords n) == 27 = "." ++ show (fst $ coords n)
+  | used n > 100 = "#"
+  | used n == 0 = "_"
+  | otherwise = "."
+
+visualize2 :: [Node] -> String
+visualize2 = foldl (\acc n -> acc ++ visualize n) ""
+
+-- quite hardcoded for my specific input
+solve2 :: [Node] -> Int
+solve2 input = emptyToGoal + goalToStart
+         -- first we find the empty node
+   where (x0, y0) = coords $ head $ filter ((==0) . used) input
+         -- then we need the maxX
+         maxX = maximum $ map (fst. coords) input
+         -- find coords for start of wall
+         wall = coords $ head $ filter ((>100) . used) input
+         -- since we want to go above
+         wx = fst wall - 1
+         wy = snd wall
+         -- calculate moves needed to get _ to G
+         emptyToGoal = abs (x0 - wx) + abs (y0 - wy) + abs (wx - maxX) + wy
+         -- calculate moves needed to move G to S
+         goalToStart = (maxX - 1) * 5
