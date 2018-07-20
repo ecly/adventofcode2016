b5922b4a6ee7454b24b264c057dc39e3869bf90c
ecly <ecly@mailbox.org>
2018-07-20 19:34:48 +0200

    solve first part of day 24

--- day24/spelunking.hs
+++ day24/spelunking.hs
@@ -1,14 +1,9 @@
-import Data.Char (digitToInt)
-import Data.List (permutations, nub)
-import Data.Map (Map)
+import Data.Char (isDigit)
+import Data.List (permutations, nub, tails, (\\))
+import Data.Map (Map, (!))
 import Data.Set (Set)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-
-data Node = Open
-          | Closed
-          | Number Int deriving (Show, Eq)
-
 type Coord = (Int, Int)
 
 main :: IO()
@@ -16,35 +11,52 @@ main = do
     graph <- parse <$> getContents
     print (solve1 graph)
 
+parse :: String -> Map Coord Char
 parse input =
     let rows = lines input
-        parseChar '#' = Closed
-        parseChar '.' = Open
-        parseChar  n  = Number (digitToInt n)
-     in Map.fromList [((x, y), parseChar c) | (y, s) <- zip [0..] rows, (x, c) <- zip [0..] s]
+     in Map.fromList [((x, y), c) | (y, s) <- zip [0..] rows, (x, c) <- zip [0..] s]
 
-solve1 :: Map Coord Node -> Int
+pairs :: [a] -> [(a, a)]
+pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
+
+insertBoth :: (Coord, Coord) -> Int -> Map (Coord, Coord) Int -> Map (Coord, Coord) Int
+insertBoth (x,y) v m = Map.insert (y,x) v m'
+    where m' = Map.insert (x,y) v m
+
+calcAllDists :: Map Coord Char -> Map (Coord, Coord) Int
+calcAllDists graph =
+    let digitCoords = map fst $ (Map.toList . Map.filter isDigit) graph
+        allPairs = pairs digitCoords
+     in foldl (\acc -> \(f,t) -> let d = bfs graph f t in insertBoth (f,t) d acc) Map.empty allPairs
+
+solve1 :: Map Coord Char -> Int
 solve1 graph =
-    let isNumber (Number _) = True
-        isNumber _ = False
-        numbers = (Map.toList . Map.filter isNumber) graph
-     in minimum $ map (pathDist graph) (permutations $ map fst numbers)
+    let numbers = (Map.toList . Map.filter isDigit) graph
+        zero = head $ filter ((=='0') . snd) numbers
+        tailPerms = permutations $ map fst (numbers \\ [zero])
+        perms = map (fst zero:) tailPerms
+        allDists = calcAllDists graph
+     in minimum $ map (pathDist allDists) perms
 
-pathDist :: Map Coord Node -> [Coord] -> Int
-pathDist graph numbers =
+pathDist :: Map (Coord, Coord) Int -> [Coord] -> Int
+pathDist dists numbers =
     let pairs = zip numbers (tail numbers)
-     in sum $ map (\(f, t) -> bfs graph f t) pairs
+     in sum $ map (\k -> dists ! k) pairs
 
-neighbours :: Map Coord Node -> Coord -> [Coord]
-neighbours _ _ = []
+neighbours :: Map Coord Char -> Coord -> [Coord]
+neighbours graph (x,y) =
+    let potentials = [(x-1,y), (x+1,y), (x,y-1), (x,y+1)]
+        isOpen '#' = False
+        isOpen _   = True
+     in filter (\n -> isOpen $ Map.findWithDefault '#' n graph) potentials
 
-bfs :: Map Coord Node -> Coord -> Coord -> Int
+bfs :: Map Coord Char -> Coord -> Coord -> Int
 bfs graph from to = aux Set.empty [from] 0
   where
-    aux _    []       d = -1
+    aux _    []       d = 0
     aux seen frontier d
       | to `elem` frontier = d
       | otherwise = aux newSeen newFrontier d+1
         where
             newSeen = Set.union seen (Set.fromList frontier)
-            newFrontier = filter (not . (flip Set.member) seen) (frontier >>= neighbours graph)
+            newFrontier = filter (\n -> Set.notMember n seen) (nub $ frontier >>= neighbours graph)
